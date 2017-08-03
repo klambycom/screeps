a4423a66d5cfb4d392d2fec8da4b823c1665589c
klambycom <christian@klamby.com>
2017-08-03 23:39:44 +0200

    Upgraders are closer to the second source

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,8 +2,8 @@ var roleUpgrader = {
   run: function(creep) {
     if(creep.memory.fueling) {
       var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+      if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[1]);
       }
       creep.memory.fueling = creep.carry.energy < creep.carryCapacity;
     }
