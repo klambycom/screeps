acd4a3aa8386987d879e13abdcf137f2906bd8f6
klambycom <christian@klamby.com>
2017-08-03 23:39:55 +0200

    Some changes to main

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var roleHarvester = require("role.harvester");
-var roleUpgrader = require("role.upgrader");
+var roles = {
+  "harvester": require("role.harvester"),
+  "upgrader": require("role.upgrader"),
+  "builder": require("role.builder"),
+};
 
 function spawn(spawn_name, role, body, amount) {
   var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
   if (creeps.length < amount) {
     var name = Game.spawns[spawn_name].createCreep(body, undefined, {role: role, fueling: true});
 
-    if (name !== ERR_NOT_ENOUGH_ENERGY) {
+    if (name !== ERR_NOT_ENOUGH_ENERGY && name !== ERR_BUSY) {
       console.log("Spawning new " + role + " in " + spawn_name + ": " + name);
     }
   }
@@ -22,19 +25,25 @@ function freeMemory() {
   }
 }
 
+function visual(roomVisual, icon, name, row) {
+  let amount = _.filter(Game.creeps, (creep) => creep.memory.role == name).length;
+  roomVisual.text(`${icon} ${amount}`, 1, row, {align: "left"});
+}
+
 module.exports.loop = function () {
   freeMemory();
 
   spawn("Spawn1", "harvester", [WORK, CARRY, MOVE], 1);
-  spawn("Spawn1", "upgrader", [WORK, CARRY, MOVE], 3);
+  spawn("Spawn1", "upgrader", [WORK, CARRY, MOVE], 5);
+  spawn("Spawn1", "builder", [WORK, CARRY, MOVE], 4);
 
   for(var name in Game.creeps) {
     var creep = Game.creeps[name];
-    if(creep.memory.role == "harvester") {
-      roleHarvester.run(creep);
-    }
-    if(creep.memory.role == "upgrader") {
-      roleUpgrader.run(creep);
-    }
+    roles[creep.memory.role].run(creep);
   }
+
+  let roomVisual = new RoomVisual();
+  visual(roomVisual, "👨‍🌾", "harvester", 1);
+  visual(roomVisual, "👨‍🔧", "upgrader", 2);
+  visual(roomVisual, "👷", "builder", 3);
 }
